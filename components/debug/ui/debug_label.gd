305fac077efc5b963a5b61c536d33e70a4d301a7
Diego Rodrigues <diejorarr@gmail.com>
2026-03-01 08:03:06 -0600

    DebugLabel remove self only

--- components/debug/ui/debug_label.gd
+++ components/debug/ui/debug_label.gd
@@ -1,3 +1,15 @@
 @tool
 class_name DebugLabel
 extends Label
+
+func _enter_tree() -> void:
+	if Engine.is_editor_hint():
+		return
+	
+	if not OS.is_debug_build():
+		queue_free()
+		return
+	
+	if not get_tree().debug_collisions_hint:
+		queue_free()
+		return
